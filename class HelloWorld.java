cbfe8a4bb0d2090c9f3141fbbc70330ef810473b
prashantd97 <prashantkumar@apptunix.com>
2022-08-29 21:42:46 +0530

    first commit

---
+++ class HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        println("Hello World");
+    }
+
+    public static void print(String message) {
+        System.out.println(message);
+    }
+
+    public static void println(String message) {
+        System.out.println(message);
+    }
+
+}
